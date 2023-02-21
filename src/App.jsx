b4a318d1c399187e4eea19724700d396193f0998
Jay <jaykapadia389@gmail.com>
2023-02-21 15:05:39 +0530

    completed frame of calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div id="calculator">
+      <div id="display">
+
+      </div>
+
+      <div id="keys">
+        <button id="" class="key jumbo"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key small"></button>
+        <button id="" class="key vertical"></button>
+        <button id="" class="key jumbo"></button>
+        <button id="" class="key small"></button>
+
+
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default App
